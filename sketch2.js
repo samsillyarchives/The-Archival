43efdff3b817ec8e173fafd375cc1e32fc7ce281
samsillyarchives <sml9740@nyu.edu>
2026-01-08 19:53:46 -0600

    adding photos

--- sketch2.js
+++ sketch2.js
@@ -2,7 +2,7 @@ let colorArray;
 
 function setup() {
    frameRate(3);
-   let elemente=createCanvas(400, 1300);
+   let elemente=createCanvas(400, 1500);
     elemente.parent('P5canvas2');
 
     colorArray=[color('#3f5a4e'),color('#efb9cd'),color('#1f1814'),color('#9f815b'), color('#e7ded9')]
